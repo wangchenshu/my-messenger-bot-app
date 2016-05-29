dc18021db591947a123226699a56bab1ee577fb1
Walter Wang <csw.dawm@gmail.com>
2016-05-29 23:43:18 +0800

    add message.py

---
+++ message.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+
+image_link = {
+    "h4": "https://s3-ap-northeast-1.amazonaws.com/walter-s3/line-bot/image/h4-logo",
+    "emacs": "https://s3-ap-northeast-1.amazonaws.com/walter-s3/line-bot/image/emacs-logo"
+}
+
+send_text = {
+    "default": "Welcome to h4!",
+    "h4": "Welcome to h4!",
+    "emacs": "Welcome to Emacs Taiwan!",
+    "how_are_you_today": "How are you today?",
+    "where_is_h4": u"""
+        餐廳：田中園光華店
+        地址：台北市中正區臨沂街 1 號
+        (捷運忠孝新生站一號出口直走第一個路口右轉)
+        時間：7:30pm ~ 10:00pm
+
+        Restaurant : 田中園 (Tian Jung Yuan)
+        Venue : No. 1, Linyi St, Zhongzheng District, Taipei City
+        (MRT JungXiao Xingshen Station Exit 1)
+        Time : 7:30pm ~ 10:00pm
+    """,
+    "what_are_h4_people_do": u"""
+        1. 討論 web, network, programming, system, blablah….
+        2. 交流系統工具 & 使用技巧
+        3. 八卦
+    """,
+    "h4_beginning": u"""
+        Hacking Thursday 是由幾位居住於台北地區的自由軟體/開放原碼開發者所發起，
+        每週四晚上會於特定咖啡店聚會。以非會議形式、交換並實做各自提出的想法，
+        輕鬆的會議過程以禮貌、謙遜與尊重的互信態度接納並鼓勵概念發想、發起新計畫、
+        並從開發者的協同開發與經驗分享中互相學習。
+    """,
+    "contact_us": u"""
+        除了實體聚會外，我們使用 Google group / Facebook group 做為大家的溝通聯絡管道。
+        聊天，討論，及聚會通告都會在這裡發佈。如果您對我們的聚會有興趣，隨時都歡迎您加入/訂閱我們的討論區，和我們交流！！
+
+        http://groups.google.com/group/hackingthursday ( Google group )
+        http://www.facebook.com/groups/hackingday/ ( Facebook group )
+        https://www.meetup.com/hackingthursday/ ( Meetup )
+    """
+}
--- webhook.py
+++ webhook.py
@@ -7,6 +7,7 @@ import urllib
 from tornado.httpclient import AsyncHTTPClient, HTTPRequest
 from tornado.options import define, options
 from tornado import gen
+import message
 
 define("verify_token", default="", help="", type=str)
 define("access_token", default="", help="", type=str)
@@ -33,21 +34,38 @@ class WebhookHandler(tornado.web.RequestHandler):
         for event in messaging_events:
             print(event["sender"]["id"])
             if event["message"] and event["message"]["text"]:
-                text = event["message"]["text"]
-                print text
+                content_text = event["message"]["text"]
+                print content_text
 
             #print(options.access_token)
             headers = { 
-                'Content-Type': 'application/json',
-                'access_token': options.access_token        
+                'Content-Type': 'application/json'
             }        
 
+            send_text = message.send_text["default"]
+
+            if "h4" in content_text and                                                       \
+               ("where" in content_text or "go" in content_text or "going" in content_text or \
+                u"如何去" in content_text or u"怎麼去" in content_text or                     \
+                u"如何走" in content_text or u"怎麼走" in content_text):
+                send_text = message.send_text["where_is_h4"]
+
+            elif "h4" in content_text and ("doing" in content_text or u"做什麼" in content_text):
+                send_text = message.send_text["what_are_h4_people_do"]
+
+            elif "h4" in content_text and u"由來" in content_text:
+                send_text = message.send_text["h4_beginning"]
+
+            elif "contact" in content_text or u"聯絡" in content_text:
+                send_text = message.send_text["contact_us"]
+
             send_data = {
+                'access_token': options.access_token,
                 "recipient": {
                     "id": event["sender"]["id"]
                 },
                 "message": {
-                    "text": text
+                    "text": send_text
                 }
             }
             
@@ -55,9 +73,12 @@ class WebhookHandler(tornado.web.RequestHandler):
             #data = urllib.urlencode(send_data)
 
             try:
-                send_message_response = yield http_client.fetch(
-                    HTTPRequest("https://graph.facebook.com/v2.6/me/messages", 'POST', headers, body=json.dumps(send_data))
-                )
+                send_message_response = yield http_client.fetch(HTTPRequest(
+                    "https://graph.facebook.com/v2.6/me/messages?access_token=" + options.access_token,
+                    'POST',
+                    headers,
+                    body=json.dumps(send_data)
+                ))
 
                 if send_message_response.error:
                     print "Error:", send_message_response.error
